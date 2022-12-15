143df0767675332ca7a4e6a2b28451f5858395ba
Tim <timtran007@gmail.com>
2022-12-14 22:09:11 -0600

    create dogs table with name, breed, personality, and dog_img_url columns

--- db/migrate/20221215040659_create_dogs.rb
+++ db/migrate/20221215040659_create_dogs.rb
@@ -1,4 +1,10 @@
 class CreateDogs < ActiveRecord::Migration[6.1]
   def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+      t.string :personality
+      t.string :dog_img_url
+    end
   end
 end
