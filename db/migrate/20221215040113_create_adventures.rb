e2757f63839c93deb7e992cd290efc03950feeaf
Tim <timtran007@gmail.com>
2022-12-14 22:06:19 -0600

    create adventures table with columns - image_url, date, name_of_place, location, rating, notes, dog_id and created_date & updated_date

--- db/migrate/20221215040113_create_adventures.rb
+++ db/migrate/20221215040113_create_adventures.rb
@@ -1,4 +1,14 @@
 class CreateAdventures < ActiveRecord::Migration[6.1]
   def change
+    create_table :adventures do |t|
+      t.string :image_url
+      t.date :date
+      t.string :name_of_place
+      t.string :location
+      t.integer :rating
+      t.string :notes
+      t.timestamps
+      t.integer :dog_id
+    end
   end
 end
