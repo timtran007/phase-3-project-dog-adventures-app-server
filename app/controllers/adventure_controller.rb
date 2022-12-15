d12780e8df1630d92d830b8730b38a40d520b81e
Tim <timtran007@gmail.com>
2022-12-14 22:32:11 -0600

    add post HTTP verb for AdventureController

--- app/controllers/adventure_controller.rb
+++ app/controllers/adventure_controller.rb
@@ -3,4 +3,15 @@ class AdventureController < ApplicationController
         adventures = Adventure.all
         adventures.to_json
     end
+
+    post '/adventures' do
+        adventure = Adventure.create(
+            image_url: params[:image_url],
+            date: params[:date],
+            name_of_place: params[:name_of_place],
+            location: params[:location],
+            rating: params[:rating],
+            dog_id: params[:dog_id]
+        )
+    end
 end
