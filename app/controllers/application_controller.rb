c60723042729d0c0d582beec1ae2ad39b91a7901
Tim <timtran007@gmail.com>
2022-12-16 00:37:56 -0600

    remove get adventures request to include in one to many association

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,55 +5,49 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+    post '/adventures' do
+        adventure = Adventure.create(
+            image_url: params[:image_url],
+            date: params[:date],
+            city: params[:city],
+            location: params[:location],
+            rating: params[:rating],
+            notes: params[:notes],
+            dog_id: params[:dog_id]
+        )
+        adventure.to_json
+    end
 
-  get '/adventures' do
-    adventures = Adventure.all
-    adventures.to_json
-end
+    patch '/adventures/:id' do
+        binding.pry
+        adventure = Adventure.find(params[:id])
+        adventure.update(
+            date: params[:date],
+            rating: params[:rating],
+            notes: params[:notes]
+        )
+        adventure.to_json
+    end
 
-post '/adventures' do
-    adventure = Adventure.create(
-        image_url: params[:image_url],
-        date: params[:date],
-        city: params[:city],
-        location: params[:location],
-        rating: params[:rating],
-        notes: params[:notes],
-        dog_id: params[:dog_id]
-    )
-    adventure.to_json
-end
+    delete '/adventures/:id' do
+        adventure = Adventure.find(params[:id])
+        adventure.destroy
+        adventure.to_json
+    end
 
-patch '/adventures/:id' do
-    binding.pry
-    adventure = Adventure.find(params[:id])
-    adventure.update(
-        date: params[:date],
-        rating: params[:rating],
-        notes: params[:notes]
-    )
-    adventure.to_json
-end
+    get '/dogs' do
+        dogs = Dog.all
+        dogs.to_json
+    end
 
-delete '/adventures/:id' do
-    adventure = Adventure.find(params[:id])
-    adventure.destroy
-    adventure.to_json
-end
-
-get '/dogs' do
-    dogs = Dog.all
-    dogs.to_json
-end
-
-post '/dogs' do
-    dog = Dog.create(
-        name: params[:name],
-        breed: params[:breed],
-        personality: params[:personality],
-        img_url: params[:img_url]
-    )
-    dog.to_json
-end
+    post '/dogs' do
+        dog = Dog.create(
+            name: params[:name],
+            breed: params[:breed],
+            personality: params[:personality],
+            img_url: params[:img_url]
+        )
+        dog.to_json
+    end
 
 end
